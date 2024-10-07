bfeab441acc9cb5310eefafd2a4a752dd2954336
Nurmuhammed <muki2022@mail.ru>
2024-10-07 08:55:53 +0600

    a

--- config/settings.py
+++ config/settings.py
@@ -168,6 +168,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
